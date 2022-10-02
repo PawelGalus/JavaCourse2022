e8f9feecc5876998480af5416c8141c23a56a6ad
Paweł Galus <pawel.galus92@gmail.com>
2022-10-02 11:39:12 +0200

    Create repository

---
+++ src/main/java/org/pawelgalus/javacourse2022/App.java
@@ -0,0 +1,18 @@
+package org.pawelgalus.javacourse2022;
+
+
+/**
+ * Course: Java Programming for Beginners - Full Course
+ */
+
+public class App {
+
+    public static void main(String[] args) {
+        int age;
+
+        age = 27;
+
+        System.out.println("I am " + age + " years old.");
+    }
+
+}
